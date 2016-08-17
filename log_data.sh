c5ed17d47fa84019d6011a84fe09a0893f9f6618
rhdzmota <rohdzmota@gmail.com>
2016-08-17 15:06:54 -0500

    create bash script to run the program easily from the bash shell

---
+++ log_data.sh
@@ -0,0 +1,7 @@
+
+echo ---------------------- DRIVING RECORDS - LOG DATA ----------------------
+echo ________________________________________________________________________	
+
+echo Reading log_data.py file... 
+
+python3 log_data.py
