1e8054b17d73acc36ea999e9033da8c77f564abe
rhdzmota <rohdzmota@gmail.com>
2016-08-17 13:06:19 -0500

    create a python script to determine the current date and time

---
+++ get_time.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from pytz import timezone, all_timezones
+
+def get_time():
+    '''
+    The function get_time() returns a date object 'now_here' with
+    the date and time information in 'America/MexicoCity'.
+    
+    Use as following:
+    >> now = get_time()
+    >> print(str(now))
+    '''
+    
+    now_utc = datetime.now(timezone('UTC'))
+    
+    now_here = now_utc.astimezone(timezone('America/Mexico_City'))
+    return now_here
+
+def show_timezones():
+    '''
+    	The show_timezones() functions prints all the available timezones in pytz
+    all_timezones module. 
+    
+    Use as following:
+    >> show_timezones()
+    '''
+    print('Total available timezones:',	len(all_timezones))
+    for zone in all_timezones:
+        #if 'Mexico	' in zone:
+        #    print(	zone)
+        print(zone)
+        
+
