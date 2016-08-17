0bb6e82e90435e9f2c23b9ed807ec9ce08e47d3d
rhdzmota <rohdzmota@gmail.com>
2016-08-17 15:06:12 -0500

    create python script to increase availability of some variables

---
+++ get_variables.py
@@ -0,0 +1,13 @@
+'''
+Funtions to make available the data for routes and weather conditions. 
+'''
+def route_options():
+    available_routes = {'1':'home_university_p','2':'university_home_p'}
+    return available_routes
+    
+def weather_options():
+    possible_cond = {'1':'Light Rain','2':'No Rain','3':'Heavy Rain'}
+    return possible_cond
+    
+    
+    
