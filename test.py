430e936160b303aae523f4a260107d0fc4d46450
Rodrigo Hernández Mota <rohdzmota@gmail.com>
2016-08-18 14:48:10 -0500

    just create a test file

---
+++ test.py
@@ -0,0 +1,13 @@
+
+'''
+Python script just to test ideas... (mostly learning)
+'''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data.csv')
+
+print(data)
+print('All right!')
