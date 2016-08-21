6760e885fe95606681f9aadc2a97628e67e34013
rhdzmota <rohdzmota@gmail.com>
2016-08-20 23:52:42 -0500

    change wrong id

--- get_variables.py
+++ get_variables.py
@@ -3,7 +3,7 @@ Functions to make available the data for routes and weather conditions.
 '''
 def route_options():
     available_routes = {'1':'home_university_p','2':'university_home_p',
-        3:'home_atrh_u',4:'atrh_home_u',5:'university_atrh_u'}
+        '3':'home_atrh_u','4':'atrh_home_u','5':'university_atrh_u'}
     return available_routes
     
 def weather_options():
