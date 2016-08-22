67d7345bc9c3b1cb54aeab241b3e1ee8b11b5303
Rodrigo Hernández Mota <rohdzmota@gmail.com>
2016-08-21 23:24:44 -0500

    personalized hex-color function

---
+++ color_hex.py
@@ -0,0 +1,8 @@
+
+def color_hex(i):
+    color_hex = ['#008744','#0057e7','#d62d20','#ffa700', #Googlestyle
+        '#009688','#011f4b','#740001','#b3cde0',
+        '#eeba30','#ff6f69','#ffeead','#88d8b0',
+        '#5a5255','#ae5a41','#03396c','#83d0c9']
+    return color_hex[i]
+
